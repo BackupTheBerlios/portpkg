57d1f0a6daa541bd093b3a4800389ece7edfb327
topf <topf>
2004-12-11 13:03:13 +0000

    Updated gnome/gnome-vfs to 2.8.3-i486-2tom.

--- ports/gnome/gnome-vfs/doinst.sh
+++ ports/gnome/gnome-vfs/doinst.sh
@@ -1,13 +1,6 @@
-config()
+inst()
 {
   case $1 in
-  *.new)
-    new=`echo $1 | sed "s,\.new$,,"`
-    if [ ! -f $new ]; then
-      mv $1 $new
-    elif cmp -s $1 $new; then
-      rm $1
-    fi;;
   *.omf)
     scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
   *.schemas)
