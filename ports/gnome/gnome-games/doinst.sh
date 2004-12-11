7281d2c7792a0479f334d74801ee207389bd4c09
topf <topf>
2004-12-11 13:07:44 +0000

    Updated gnome/gnome-games to 2.8.2-i486-1tom.

--- ports/gnome/gnome-games/doinst.sh
+++ ports/gnome/gnome-games/doinst.sh
@@ -1,17 +1,20 @@
-config()
+inst()
 {
   case $1 in
-    *.new)
-      new=`echo $1 | sed "s,\.new$,,"`
-      if [ ! -f $new ]; then
-        mv $1 $new
-      elif cmp -s $1 $new; then
-        rm $1
-      fi;;
-    *.omf)
-      scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
-    *.schemas)
-      GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-      gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
+  *.omf)
+    scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
+  *.schemas)
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
   esac
 }
+if [ -d var/lib/games/tmp ]; then
+  ( cd var/lib/games/tmp
+    for file in * ; do
+      if [ ! -r ../$file ]; then
+        mv $file ..
+      fi
+    done )
+  rm -rf var/lib/games/tmp
+fi
+                                
