0003941009c0be27eeaa039e031dfe3dfed22fd3
topf <topf>
2005-02-26 01:14:23 +0000

    Moved open/gpass-0.4.4-i486-1lem to gnome/.

--- ports/gnome/gpass/doinst.sh
+++ ports/gnome/gpass/doinst.sh
@@ -1,17 +1,1 @@
-config()
-{
-  case $1 in
-    *.new)
-      new=`echo $1 | sed "s,\.new$,,"`
-      if [ ! -f $new ]; then
-        mv $1 $new
-      elif cmp -s $1 $new; then
-        rm $1
-      fi;;
-    *.omf)
-      scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
-    *.schemas)
-      GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-      gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
-  esac
-}
+/usr/bin/update-desktop-database /usr/share/applications
