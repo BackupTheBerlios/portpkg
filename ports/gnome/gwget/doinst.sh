41f047f7f5137e4f321acf0528716ef4d1630ebe
topf <topf>
2005-02-26 01:15:57 +0000

    Moved open/gwget-0.93-i486-1lem to gnome/.

---
+++ ports/gnome/gwget/doinst.sh
@@ -0,0 +1,12 @@
+inst()
+{
+  case $1 in
+    *.omf)
+      scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
+    *.schemas)
+      GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+      gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
+  esac
+}
+
+update-desktop-database /usr/share/applications
