d31154317a36a22a8d89a9962ea2639ac170bafb
topf <topf>
2005-03-07 18:20:49 +0000

    Updated gnome/mail-notification to 1.1-i686-1tom.

--- ports/gnome/mail-notification/doinst.sh
+++ ports/gnome/mail-notification/doinst.sh
@@ -1,17 +1,6 @@
-config()
-{
-  case $1 in
-    *.new)
-      new=`echo $1 | sed "s,\.new$,,"`
-      if [ ! -f $new ]; then
-        mv $1 $new
-      elif cmp -s $1 $new; then
-        rm $1
-      fi;;
-    *.omf)
-      scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
-    *.schemas)
-      GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-      gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
-  esac
-}
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
