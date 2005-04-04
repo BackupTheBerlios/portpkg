6f7ef4a49e8eda79226db4f79cd236f9652d4c7d
topf <topf>
2005-04-04 12:37:34 +0000

    Release 0.37.

--- ports/d/portpkg/doinst.sh
+++ ports/d/portpkg/doinst.sh
@@ -1,10 +1,23 @@
-# ok this is a hot-fix for 0.35cvs-7tom/8tom:
-if ! [ -c dev/stdout ] && ! [ -L dev/stdout ]; then
-  # stdout is not a character device file or a symlink to one
-  # move what's left to root
-  mv dev/stdout root/
-  # symlink to fd/1
-  ln -s fd/1 dev/stdout
-  echo "FIXED /DEV/STDOUT, PLEASE CHECK IT:" >&2
-  ls -l dev/stdout >&2
-fi
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+config etc/portpkg/exclude.new
+
+## ok this is a hot-fix for 0.35cvs-7tom/8tom:
+#if ! [ -c dev/stdout ] && ! [ -L dev/stdout ]; then
+#  # stdout is not a character device file or a symlink to one
+#  # move what's left to root
+#  mv dev/stdout root/
+#  # symlink to fd/1
+#  ln -s fd/1 dev/stdout
+#  echo "FIXED /DEV/STDOUT, PLEASE CHECK IT:" >&2
+#  ls -l dev/stdout >&2
+#fi
