139e7972505180d2272f27c04169b837038fed63
topf <topf>
2005-04-16 18:36:45 +0000

    Updated gnome/gnome-terminal to 2.10.0-i486-2tom.

--- ports/gnome/gnome-terminal/doinst.sh
+++ ports/gnome/gnome-terminal/doinst.sh
@@ -1,5 +1,5 @@
 if [ -x usr/bin/scrollkeeper-update ]; then
-  usr/bin/scrollkeeper-update >/dev/null 2>&1
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
 fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
