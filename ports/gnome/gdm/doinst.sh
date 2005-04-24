81064be5069e4d4d0b4ac9397fe3ffcb1cf1fa38
topf <topf>
2005-04-24 12:49:42 +0000

    version bump (lem)

---
+++ ports/gnome/gdm/doinst.sh
@@ -0,0 +1,20 @@
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
+
+config etc/X11/gdm/gdm.conf.new
