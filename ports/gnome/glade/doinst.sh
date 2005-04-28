9873360013fa0be407efc6b4aafe8a1d55cc59e3
topf <topf>
2005-04-28 13:29:01 +0000

    new (lem)

---
+++ ports/gnome/glade/doinst.sh
@@ -0,0 +1,6 @@
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
