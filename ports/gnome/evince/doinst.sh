488ce2da0e155c457338e4ca91647b30d28724ae
topf <topf>
2005-04-28 13:27:15 +0000

    New entry. (tom)

---
+++ ports/gnome/evince/doinst.sh
@@ -0,0 +1,16 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
+gconf etc/gconf/schemas/evince-thumbnailer-djvu.schemas
+gconf etc/gconf/schemas/evince-thumbnailer-dvi.schemas
+gconf etc/gconf/schemas/evince-thumbnailer.schemas
+gconf etc/gconf/schemas/evince.schemas
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
