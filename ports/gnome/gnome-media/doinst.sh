298ce337f4ed0aa4898567c6d80e1d815a72c087
topf <topf>
2005-05-01 10:42:02 +0000

    Updated to 2.10.2.

--- ports/gnome/gnome-media/doinst.sh
+++ ports/gnome/gnome-media/doinst.sh
@@ -1,10 +1,16 @@
-inst()
-{
-  case $1 in
-  *.omf)
-    scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
-  *.schemas)
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
     GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
-  esac
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
 }
+gconf etc/gconf/schemas/CDDB-Slave2.schemas
+gconf etc/gconf/schemas/gnome-audio-profiles.schemas
+gconf etc/gconf/schemas/gnome-cd.schemas
+gconf etc/gconf/schemas/gnome-sound-recorder.schemas
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
