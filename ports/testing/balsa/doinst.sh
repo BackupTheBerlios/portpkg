4a8f3fa3de03c309f77baafc4aa532ba9de9ab92
topf <topf>
2005-05-15 18:20:06 +0000

    Updated to 2.3.2.

--- ports/testing/balsa/doinst.sh
+++ ports/testing/balsa/doinst.sh
@@ -1,1 +1,6 @@
-usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
