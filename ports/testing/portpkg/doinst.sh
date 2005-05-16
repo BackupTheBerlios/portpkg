233cad12862f357584e1d94c7679898f01d79c70
topf <topf>
2005-05-16 12:11:45 +0000

    Next try. (tom)

---
+++ ports/testing/portpkg/doinst.sh
@@ -0,0 +1,29 @@
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+#config etc/portpkg/exclude.new
+config etc/portpkg/repos.new
+
+#if [ -x etc/portpkg/importers/pp-sync-bin ]; then
+#  chmod 755 etc/portpkg/importers/pp-sync-bin.new
+#fi
+#mv etc/portpkg/importers/pp-sync-bin{.new,}
+
+## ok this is a hot-fix for 0.35cvs-7tom/8tom:
+#if ! [ -c dev/stdout ] && ! [ -L dev/stdout ]; then
+#  # stdout is not a character device file or a symlink to one
+#  # move what's left to root
+#  mv dev/stdout root/
+#  # symlink to fd/1
+#  ln -s fd/1 dev/stdout
+#  echo "FIXED /DEV/STDOUT, PLEASE CHECK IT:" >&2
+#  ls -l dev/stdout >&2
+#fi
