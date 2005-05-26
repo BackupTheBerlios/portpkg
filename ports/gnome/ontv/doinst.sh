ff450d0aff6475cf5dbd09cf6ad05179095e8129
topf <topf>
2005-05-26 09:33:27 +0000

    next try after exporting failure (lem)

---
+++ ports/gnome/ontv/doinst.sh
@@ -0,0 +1,7 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
+gconf etc/gconf/schemas/ontv.schemas
