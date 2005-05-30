de79e12e70412c867e5ff0e368ade38f9e5a8f96
topf <topf>
2005-05-30 00:13:58 +0000

    Version update.

--- ports/gnome/gnome-btdownload/doinst.sh
+++ ports/gnome/gnome-btdownload/doinst.sh
@@ -1,3 +1,9 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
 if [ -x usr/bin/update-mime-database ]; then
   usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
 fi
