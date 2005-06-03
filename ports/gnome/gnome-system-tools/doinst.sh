69c9db382539dcbac85d7a66b6903dbf2ce8c354
topf <topf>
2005-06-03 08:42:31 +0000

    next try after exporting failure (lem)

--- ports/gnome/gnome-system-tools/doinst.sh
+++ ports/gnome/gnome-system-tools/doinst.sh
@@ -1,3 +1,13 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
+gconf etc/gconf/schemas/gnome-system-tools.schemas
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
 fi
