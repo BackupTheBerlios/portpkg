2af1e70a84c5c4d954842e5e1e839977defaa425
topf <topf>
2005-06-04 09:34:15 +0000

    Gconf installation fixes.

--- ports/gnome/metacity/doinst.sh
+++ ports/gnome/metacity/doinst.sh
@@ -1,3 +1,10 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
+gconf etc/gconf/schemas/metacity.schemas
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
