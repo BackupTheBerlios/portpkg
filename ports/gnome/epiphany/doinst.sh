6e5ecec895066ded85cd4e03dc11920fa18268c1
topf <topf>
2005-06-04 09:19:42 +0000

    Version update.

--- ports/gnome/epiphany/doinst.sh
+++ ports/gnome/epiphany/doinst.sh
@@ -1,3 +1,11 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
+gconf etc/gconf/schemas/epiphany-lockdown.schemas
+gconf etc/gconf/schemas/epiphany.schemas
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
 fi
