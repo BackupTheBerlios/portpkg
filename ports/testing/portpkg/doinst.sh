cfd7a42c1a2445447497e661f4c9c663467ed6ed
topf <topf>
2005-06-09 07:38:30 +0000

    Release 0.38test10.

--- ports/testing/portpkg/doinst.sh
+++ ports/testing/portpkg/doinst.sh
@@ -1,31 +1,12 @@
-config() {
-  NEW="$1"
-  OLD="`dirname $NEW`/`basename $NEW .new`"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-#config etc/portpkg/exclude.new
-#config etc/portpkg/repos.new
-
-for i in pseudo-ports slackware-source; do
+# keep file mask of importers
+for i in portpkg-tree pseudo-ports slackware-source; do
   if [ -x etc/portpkg/importers/$i ]; then
     chmod 755 etc/portpkg/importers/$i.new
   fi
   mv etc/portpkg/importers/$i{.new,}
 done
 
-## ok this is a hot-fix for 0.35cvs-7tom/8tom:
-#if ! [ -c dev/stdout ] && ! [ -L dev/stdout ]; then
-#  # stdout is not a character device file or a symlink to one
-#  # move what's left to root
-#  mv dev/stdout root/
-#  # symlink to fd/1
-#  ln -s fd/1 dev/stdout
-#  echo "FIXED /DEV/STDOUT, PLEASE CHECK IT:" >&2
-#  ls -l dev/stdout >&2
-#fi
+# export.http is default
+if ! [ -f etc/portpkg/export ]; then
+  ln -s export.http etc/portpkg/export
+fi
