a189f856d4187931ac284a36f24b3cc2a4199669
topf <topf>
2005-07-08 08:56:39 +0000

    Version update.

--- ports/gnome/totem/doinst.sh
+++ ports/gnome/totem/doinst.sh
@@ -7,6 +7,9 @@ gconf() {
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
 fi
+if [ -x usr/bin/update-mime-database ]; then
+  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
