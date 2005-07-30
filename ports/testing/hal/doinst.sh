bb8ad8f84054370165ab97a7187f880d76a99e01
topf <topf>
2005-07-30 13:12:12 +0000

    version update (lem)

--- ports/testing/hal/doinst.sh
+++ ports/testing/hal/doinst.sh
@@ -1,3 +1,13 @@
-#id=haldaemon
-#grep -q ^$id: etc/group || groupadd $id
-#grep -q ^$id: etc/passwd || useradd $id -g $id
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/rc.d/rc.hald.new
