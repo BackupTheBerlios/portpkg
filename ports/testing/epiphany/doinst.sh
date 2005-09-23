7088832e515085e7a79a23a034d19ecbbd546685
topf <topf>
2005-09-23 12:11:15 +0000

    Removed from testing/.

--- ports/testing/epiphany/doinst.sh
+++
@@ -1,9 +0,0 @@
-gconf() {
-  if [ -x usr/bin/gconftool-2 ]; then
-    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
-  fi
-}
-if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database >/dev/null 2>&1
-fi
