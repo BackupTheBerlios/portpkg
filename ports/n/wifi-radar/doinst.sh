43aa9cb029de2597ad6bc730351ee6f44be8ba50
topf <topf>
2005-11-12 10:42:29 +0000

    Moved to n/.

--- ports/n/wifi-radar/doinst.sh
+++ ports/n/wifi-radar/doinst.sh
@@ -1,13 +1,13 @@
 config() {
   NEW="$1"
   OLD="`dirname $NEW`/`basename $NEW .new`"
-# If there's no config file by that name, mv it over:
+  # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
   elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
     rm $NEW
   fi
-# Otherwise, we leave the .new copy for the admin to consider...
+  # Otherwise, we leave the .new copy for the admin to consider...
 }
 
 if [ -x usr/bin/update-desktop-database ]; then                                 
