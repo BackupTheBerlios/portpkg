217050f45a1e0f190d496e76b58de4f3c318cd1c
topf <topf>
2006-03-16 14:30:58 +0000

    Moved from patches/.

--- ports/pasture/gtk+2/doinst.sh
+++ ports/pasture/gtk+2/doinst.sh
@@ -1,3 +1,7 @@
 mkdir -p etc/gtk-2.0
-usr/bin/gtk-query-immodules-2.0 >etc/gtk-2.0/gtk.immodules
-usr/bin/gdk-pixbuf-query-loaders >etc/gtk-2.0/gdk-pixbuf.loaders
+if [ -x usr/bin/gtk-query-immodules-2.0 ]; then
+  usr/bin/gtk-query-immodules-2.0 >etc/gtk-2.0/gtk.immodules
+fi
+if [ -x usr/bin/gdk-pixbuf-query-loaders ]; then
+  usr/bin/gdk-pixbuf-query-loaders >etc/gtk-2.0/gdk-pixbuf.loaders 2>&1
+fi
