874a742c273c97434a58d085251bf8236c6405b4
topf <topf>
2006-03-17 19:26:25 +0000

    Version bump.

--- ports/gnome/eog/doinst.sh
+++ ports/gnome/eog/doinst.sh
@@ -1,10 +1,12 @@
-inst()
-{
-  case $1 in
-  *.omf)
-    scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
-  *.schemas)
+gconf() {
+  if [ -x /usr/bin/gconftool-2 ]; then
     GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
-  esac
+    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
 }
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
