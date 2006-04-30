99ae4a530d45dbc81501b583dfb62a95b384966f
topf <topf>
2006-04-30 11:03:20 +0000

    Version bump.

--- ports/gnome/gst-plugins-ugly/doinst.sh
+++ ports/gnome/gst-plugins-ugly/doinst.sh
@@ -2,5 +2,3 @@ if [ -x usr/bin/gst-register ]; then
   sbin/ldconfig >/dev/null 2>&1
   usr/bin/gst-register >/dev/null 2>&1
 fi
-#gconf etc/gconf/schemas/gstreamer-0.8.schemas
-#gconf etc/gconf/schemas/gstreamer.schemas
