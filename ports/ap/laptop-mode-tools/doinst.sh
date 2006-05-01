3b8ea93353458adb7aff573aab35b404d48d48c1
topf <topf>
2006-05-01 07:34:05 +0000

    Added config() to install config files.

--- ports/ap/laptop-mode-tools/doinst.sh
+++ ports/ap/laptop-mode-tools/doinst.sh
@@ -1,3 +1,17 @@
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+config etc/laptop-mode/laptop-mode.conf.new
+config etc/laptop-mode/lm-profiler.conf.new
+config etc/rc.d/rc.laptop-mode.new
 if ! grep -sq "/etc/rc.d/rc.laptop-mode" etc/rc.d/rc.local; then
 cat <<EOF >>etc/rc.d/rc.local
 if [ -x /etc/rc.d/rc.laptop-mode ]; then
