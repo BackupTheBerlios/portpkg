203815603a6388ce0e17736ca59b6eda3d6e3a20
topf <topf>
2006-05-04 19:34:35 +0000

    Updates.

--- ports/d/portpkg/doinst.sh
+++ ports/d/portpkg/doinst.sh
@@ -9,20 +9,9 @@ config() {
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
-#config etc/portpkg/exclude.new
-
-#if [ -x etc/portpkg/importers/pp-sync-bin ]; then
-#  chmod 755 etc/portpkg/importers/pp-sync-bin.new
-#fi
-#mv etc/portpkg/importers/pp-sync-bin{.new,}
-
-## ok this is a hot-fix for 0.35cvs-7tom/8tom:
-#if ! [ -c dev/stdout ] && ! [ -L dev/stdout ]; then
-#  # stdout is not a character device file or a symlink to one
-#  # move what's left to root
-#  mv dev/stdout root/
-#  # symlink to fd/1
-#  ln -s fd/1 dev/stdout
-#  echo "FIXED /DEV/STDOUT, PLEASE CHECK IT:" >&2
-#  ls -l dev/stdout >&2
-#fi
+config etc/portpkg/repos.new
+config etc/portpkg/mirrors.new
+config etc/portpkg/ignore.new
+config etc/portpkg/exclude.new
+config etc/portpkg/use.new
+config etc/portpkg/virtual.new
