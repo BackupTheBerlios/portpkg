f1ed1ea7f0a45ff73fff17b9eede49515be4291b
topf <topf>
2006-06-18 10:08:00 +0000

    Version bump.

--- ports/xap/kino/doinst.sh
+++ ports/xap/kino/doinst.sh
@@ -1,6 +1,3 @@
-if [ -x usr/bin/update-mime-database ]; then
-  usr/bin/update-mime-database usr/share/mime 2>&1
-fi
 if [ -x usr/bin/update-desktop-database ]; then
-  usr/bin/update-desktop-database 2>&1
+  usr/bin/update-desktop-database >/dev/null 2>&1
 fi
