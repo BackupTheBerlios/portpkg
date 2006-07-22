cff8dc31b1692e518455b37bb37e490d7ff0b6fd
topf <topf>
2006-07-22 07:00:59 +0000

    Upgraded to gDesklets-0.35.3. Fixed URL.

--- ports/gnome/gdesklets/doinst.sh
+++ ports/gnome/gdesklets/doinst.sh
@@ -1,9 +1,6 @@
-gconf() {
-  if [ -x usr/bin/gconftool-2 ]; then
-    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-    usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
-  fi
-}
+if [ -x usr/bin/update-mime-database ]; then
+  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
