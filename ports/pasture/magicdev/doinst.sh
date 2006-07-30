84b8b98cbb478ec2a83be5429f05a71d38b4084c
topf <topf>
2006-07-30 07:53:14 +0000

    Fixed gconf installation. Patched .desktop entry to be viewed in recent GNOME versions.

--- ports/pasture/magicdev/doinst.sh
+++ ports/pasture/magicdev/doinst.sh
@@ -1,3 +1,9 @@
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
+    GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
+    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
+}
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
