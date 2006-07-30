cace0789173f687f0e31f7ad092db6fe4db6c4a7
topf <topf>
2006-07-30 13:52:57 +0000

    Fixed updating of icon cache.

--- ports/xap/tango-icon-theme/doinst.sh
+++ ports/xap/tango-icon-theme/doinst.sh
@@ -1,5 +1,5 @@
 icons() {
   if [ -x usr/bin/gtk-update-icon-cache ]; then
-    usr/bin/gtk-update-icon-cache `dirname $1` >/dev/null 2>&1
+    usr/bin/gtk-update-icon-cache -f `dirname $1` >/dev/null 2>&1
   fi
 }
