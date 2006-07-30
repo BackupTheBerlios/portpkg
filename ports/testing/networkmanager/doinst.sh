8f15930b4c89229d04a60bce35672f6f2302f4a1
topf <topf>
2006-07-30 07:33:25 +0000

    Added NetworkManager-0.6.4.

---
+++ ports/testing/networkmanager/doinst.sh
@@ -0,0 +1,23 @@
+config() {
+  NEW="$1"
+  OLD="`dirname $NEW`/`basename $NEW .new`"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "`cat $OLD | md5sum`" = "`cat $NEW | md5sum`" ]; then # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+if ! grep -sq "/etc/rc.d/rc.networkmanager start" etc/rc.d/rc.local; then
+  cat <<EOF >>etc/rc.d/rc.local
+
+if [ -x /etc/rc.d/rc.networkmanager ]; then
+  /sbin/dhcdbd -s
+  /etc/rc.d/rc.networkmanager start
+fi
+EOF
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
