a9b4fe19130735988ac5cd052c511c404738e59c
topf <topf>
2006-08-09 10:09:55 +0000

    Added gtk-immodules update to doinst.sh.

--- ports/xap/scim/doinst.sh
+++ ports/xap/scim/doinst.sh
@@ -1,3 +1,7 @@
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
+if [ -x usr/bin/gtk-query-immodules-2.0 ]; then
+  mkdir -p etc/gtk-2.0
+  usr/bin/gtk-query-immodules-2.0 >etc/gtk-2.0/gtk.immodules
+fi
