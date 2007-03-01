98535945958379dfcb52205ac3c3f65f8ed854ad
topf <topf>
2007-03-01 10:15:17 +0000

    Upgraded to 0.4.11.

--- ports/gnome/revelation/doinst.sh
+++ ports/gnome/revelation/doinst.sh
@@ -1,14 +1,12 @@
-inst()
-{
-  case $1 in
-  *.omf)
-    scrollkeeper-update -p var/lib/scrollkeeper -o $1 >/dev/null 2>&1;;
-  *.schemas)
+gconf() {
+  if [ -x usr/bin/gconftool-2 ]; then
     GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` \
-    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1;;
-  esac
+    gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
+  fi
 }
-
-update-desktop-database /usr/share/applications
-update-mime-database /usr/share/mime
-
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-mime-database ]; then
+  usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
