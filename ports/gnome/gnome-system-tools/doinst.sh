c22bd4489af1ed5ecce9fbcafa423c074d9e3fbc
topf <topf>
2007-12-22 14:47:17 +0000

    Updated to gnome-system-tools-2.20.0.

--- ports/gnome/gnome-system-tools/doinst.sh
+++ ports/gnome/gnome-system-tools/doinst.sh
@@ -7,6 +7,9 @@ gconf() {
 if [ -x usr/bin/scrollkeeper-update ]; then
   usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
 fi
+if [ -x usr/bin/gtk-update-icon-cache ]; then
+  usr/bin/gtk-update-icon-cache -ft usr/share/icons/hicolor >/dev/null 2>&1
+fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
