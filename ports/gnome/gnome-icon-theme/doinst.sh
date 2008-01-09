14a2567727db1162a8380be41489a9a3a443783b
topf <topf>
2008-01-09 08:16:05 +0000

    Fixed postinstall. Added update-desktop-database and another gtk-update-icon-cache instruction to doinst.sh.

--- ports/gnome/gnome-icon-theme/doinst.sh
+++ ports/gnome/gnome-icon-theme/doinst.sh
@@ -3,3 +3,9 @@ icons() {
     usr/bin/gtk-update-icon-cache -ft `dirname $1` >/dev/null 2>&1
   fi
 }
+if [ -x usr/bin/gtk-update-icon-cache ]; then
+  usr/bin/gtk-update-icon-cache -ft usr/share/icons/hicolor >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database 2>/dev/null
+fi
