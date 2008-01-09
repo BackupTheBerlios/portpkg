5a45fb521564061d86b38ed0ba8d0d5b8140a7a8
topf <topf>
2008-01-09 08:06:21 +0000

    Updated to gnome-applets-2.20.0.

--- ports/gnome/gnome-applets/doinst.sh
+++ ports/gnome/gnome-applets/doinst.sh
@@ -4,9 +4,9 @@ gconf() {
     usr/bin/gconftool-2 --makefile-install-rule $1 >/dev/null 2>&1
   fi
 }
-if [ -x usr/bin/scrollkeeper-update ]; then
-  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
-fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
+fi
+if [ -x usr/bin/scrollkeeper-update ]; then
+  usr/bin/scrollkeeper-update -p var/lib/scrollkeeper >/dev/null 2>&1
 fi
