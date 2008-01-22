652faaf3c9ac52b5067cc5b8c7ec91c4b071d46b
topf <topf>
2008-01-22 19:23:50 +0000

    Finally insert the hard-earned W3C HTML-4.01 logo... clap, clap, clap

--- portpkg/htdocs/index.php
+++ portpkg/htdocs/index.php
@@ -298,6 +298,11 @@ if ($file) {
 	
 	<div id="sub">
 	<table><tr><td>
+        <p>
+            <a href="http://validator.w3.org/check?uri=referer"><img
+                src="http://www.w3.org/Icons/valid-html401"
+                alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
+        </p>
     </td><td>
         <p>Copyright &copy; 2004-2008 Portpkg Project. All rights reserved.<br>
         Slackware&reg; is a registered trademark of Patrick Volkerding.<br>
