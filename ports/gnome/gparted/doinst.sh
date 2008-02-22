6a82e9dc1dbf1d1941be83b4477d3b5481194429
topf <topf>
2008-02-22 10:33:21 +0000

    Updated to gparted-0.3.5. Fixed installation script.

--- ports/gnome/gparted/doinst.sh
+++ ports/gnome/gparted/doinst.sh
@@ -1,3 +1,12 @@
+
+if which gnomesu >/dev/null 2>&1; then
+  sed -i "/^Exec=/s,=,=gnomesu ," usr/share/applications/gparted.desktop
+elif which gksu; then
+  sed -i "/^Exec=/s,=,=gksu -u root ," usr/share/applications/gparted.desktop
+fi
+if [ -x usr/bin/gtk-update-icon-cache ]; then
+  usr/bin/gtk-update-icon-cache -ft usr/share/icons/hicolor >/dev/null 2>&1
+fi
 if [ -x usr/bin/update-desktop-database ]; then
   usr/bin/update-desktop-database >/dev/null 2>&1
 fi
