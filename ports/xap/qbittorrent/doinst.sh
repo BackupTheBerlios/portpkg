85af9c0962f2a9218fe04719ae77164c93844f17
sheva <sheva>
2008-05-05 09:15:30 +0000

    new port: qbittorrent v 1.0.0 - bittorrent client

---
+++ ports/xap/qbittorrent/doinst.sh
@@ -0,0 +1,6 @@
+if [ -x usr/bin/gtk-update-icon-cache ]; then
+  usr/bin/gtk-update-icon-cache -ft usr/share/icons/hicolor >/dev/null 2>&1
+fi
+if [ -x usr/bin/update-desktop-database ]; then
+  usr/bin/update-desktop-database >/dev/null 2>&1
+fi
